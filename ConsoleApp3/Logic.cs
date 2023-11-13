4394aca55aaef6f38162de7bb35c3644ec8e4b69
brandonshawfhl <142125786+brandonshawfhl@users.noreply.github.com>
2023-11-12 18:12:49 -0800

    created object Question
    created first Queestion firstQuestion

--- ConsoleApp3/Logic.cs
+++ ConsoleApp3/Logic.cs
@@ -2,5 +2,14 @@
 {
     internal class Logic
     {
+        public class Question
+        {
+            public string gameQuestion;
+            public string rightAnswer;
+            public List<String> wrongAnswers;
+        }
+
+        Question firstQuestion = new Question();
+
     }
 }
--- ConsoleApp3/UserInterface.cs
+++ ConsoleApp3/UserInterface.cs
@@ -2,5 +2,9 @@
 {
     internal class UserInterface
     {
+        public static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Quiz Land! Test your intelligence!");
+        }
     }
 }
