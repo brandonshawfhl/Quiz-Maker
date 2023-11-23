a63794fcbd73a2cc0ca2769475d8c2027b668cda
brandonshawfhl <brandonshawfhl@gmail.com>
2023-11-22 16:56:53 -0800

    here is partially what is supposed to be up there

--- ConsoleApp3/Logic.cs
+++ ConsoleApp3/Logic.cs
@@ -2,5 +2,16 @@
 {
     internal class Logic
     {
+        public class Question
+        {
+            public string PrintQuestion;
+            public string CorrectAnswer;
+            public List<String> WrongAnswers;
+        }
+
+        Question firstQuestion = new Question();
+
+        firstQuestion.PrintQuestion = "What color is the grass";
+
     }
 }
