66e134db17c713829e45566628b1bb8913bf34c5
brandonshawfhl <142125786+brandonshawfhl@users.noreply.github.com>
2023-11-25 18:12:56 -0800

    created class Question

--- ConsoleApp3/Logic.cs
+++ ConsoleApp3/Logic.cs
@@ -2,16 +2,9 @@
 {
     internal class Logic
     {
-        public class Question
-        {
-            public string GameQuestion;
-            public string RightAnswer;
-            public List<String> WrongAnswers;
-        }
-
-        Question firstQuestion = new Question();
         
-        firstQuestion.GameQuestion = "What color is grass?";
+
+       
 
         
 
