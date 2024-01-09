218bd803462849693a3d4dd87d132ad66a5b04c7
brandonshawfhl <brandonshawfhl@gmail.com>
2024-01-08 22:23:51 -0800

    added another while loop to program and moved quizAction initialization just beloe UserInterface.PrintWelcomeMessage()

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -9,22 +9,26 @@
             QuestionAndAnswers userQuestion = new QuestionAndAnswers();
             bool moreQuestions = true;
             UserInterface.PrintWelcomeMessage();
-           
-            while (moreQuestions)
+            int quizAction = UserInterface.PromptForQuizAction();
+
+            while (quizAction == Constants.MAKE_CHOICE)
             {
-                userQuestion.printQuestion = UserInterface.PromptForQuestion();
-                userQuestion.correctAnswer = UserInterface.PromptForCorrectAnswer();
-                List<string> answerList = new List<string>()
+
+                while (moreQuestions)
+                {
+                    userQuestion.printQuestion = UserInterface.PromptForQuestion();
+                    userQuestion.correctAnswer = UserInterface.PromptForCorrectAnswer();
+                    List<string> answerList = new List<string>()
                 {
                     userQuestion.correctAnswer
                 };
-                answerList = UserInterface.PromptForAnswers(answerList);
-                userQuestion.allAnswers = Logic.AnswerArray(answerList);
-                questionAndAnswers.Add(userQuestion);
-                moreQuestions = UserInterface.PromptForMoreQuestions();
+                    answerList = UserInterface.PromptForAnswers(answerList);
+                    userQuestion.allAnswers = Logic.AnswerArray(answerList);
+                    questionAndAnswers.Add(userQuestion);
+                    moreQuestions = UserInterface.PromptForMoreQuestions();
+                }
             }
 
-            int quizAction = UserInterface.PromptForQuizAction();
             Logic.QuizOptions(questionAndAnswers, quizAction);
             Logic.PrintQuestionAndAnswers(questionAndAnswers);
 
