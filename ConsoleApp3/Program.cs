943c1b6c6c23a5660c28b307593aaa19db21695f
brandonshawfhl <brandonshawfhl@gmail.com>
2024-01-14 17:48:41 -0800

    moved answerArray, questionList and correctAnswerList out of while loop in Program
    deleted answerList from Program and made it string[][] answerArray
    
    created bool moreQuestions in Program
    
    created MoreQuestions() in UserInterface
    
    created bool moreQuestions in MoreQuestions()
    
    removed unnessecary value assignment of moreQuizOptions in AnotherQuizOption()
    
    created questionNumber in Program
    
    moved assignment of value to all properties of madeQuiz out of while loop

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -7,22 +7,31 @@
         {
             bool anotherQuiz = true;
             userQuiz madeQuiz = new userQuiz();
+
             UserInterface.PrintWelcomeMessage();
             QuizChoice.QuizOptions quizChoice = UserInterface.PromptForQuizAction();
 
             if (quizChoice == QuizChoice.QuizOptions.Make)
             {
                 List<string> questionList = new List<string>();
-                string printQuestion = UserInterface.PromptForQuestion();
-                questionList.Add(printQuestion);
-                List<string> answerList = new List<string>();
                 List<string> correctAnswersList = new List<string>();
-                string correctAnswer = UserInterface.PromptForCorrectAnswer();
-                correctAnswersList.Add(correctAnswer);
-                madeQuiz.correctAnswers = correctAnswersList;
-                answerList = UserInterface.PromptForAnswers(correctAnswer);
-                madeQuiz.printQuestions = questionList;
-                madeQuiz.allAnswers = answerList;
+                int questionNumber = 0;
+                bool moreQuestions = true;
+                string[][] answerArray;
+                while (moreQuestions)
+                {
+                    questionNumber++;
+                    string printQuestion = UserInterface.PromptForQuestion();
+                    questionList.Add(printQuestion);
+                    string correctAnswer = UserInterface.PromptForCorrectAnswer();
+                    correctAnswersList.Add(correctAnswer);
+                    answerList = UserInterface.PromptForAnswers(correctAnswer);
+                    moreQuestions = UserInterface.MoreQuestions();
+                }
+                    madeQuiz.printQuestions = questionList;
+                    madeQuiz.correctAnswers = correctAnswersList;
+                    madeQuiz.allAnswers = answerList;
+
             }
 
             if (quizChoice == QuizChoice.QuizOptions.Load)
