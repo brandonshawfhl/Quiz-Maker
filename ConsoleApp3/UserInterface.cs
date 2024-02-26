70cdac2fd3352849114da6a88db40be87d2c7c10
brandonshawfhl <brandonshawfhl@gmail.com>
2024-02-26 03:18:27 -0800

    completely restructured GetQuestionNumber()

--- ConsoleApp3/UserInterface.cs
+++ ConsoleApp3/UserInterface.cs
@@ -282,10 +282,22 @@
 
         public static int GetQuestionNumber(List<QuizCard> currentQuiz)
         {
-            Console.WriteLine("Which question would you like to change?");
-            Console.WriteLine("Please enter the number of the question you like to change.");
-            int questionNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("This question does not exist. Please try again.");
+            int questionNumber = 0;
+            bool questionDoesntExist = questionNumber > currentQuiz.Count || questionNumber < 0;
+
+            do
+            {
+                Console.WriteLine("Which question would you like to change?");
+                Console.WriteLine("Please enter the number of the question you like to change.");
+                questionNumber = Convert.ToInt32(Console.ReadLine());
+
+                if (questionDoesntExist)
+                {
+                    Console.WriteLine("This question does not exist. Please try again.");
+                }
+            }
+            while (questionDoesntExist);
+
             return questionNumber;
         }
 
